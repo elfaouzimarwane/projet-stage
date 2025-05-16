ddb08b37377868667326b7efad10bca1c4921881
elfaouzimarwane <marwaneelfaouzi@gmail.com>
2025-05-16 15:43:07 +0100

    Update 20250501144707_create_adherent_table.js

--- backend/migrations/20250501144707_create_adherent_table.js
+++ backend/migrations/20250501144707_create_adherent_table.js
@@ -2,6 +2,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('adherent', (table) => {
       table.increments('id').primary();
       table.string('adherentNumber', 50).notNullable();
+      table.string('nom', 100).notNullable();
+      table.string('prenom', 100).notNullable();
       table.string('CIN', 50).nullable();
       table.text('visitReason').nullable();
       table.timestamp('createdAt').defaultTo(knex.fn.now());
